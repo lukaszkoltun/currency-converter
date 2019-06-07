e2a3189c12a3e2db47316d0aa175fde44701b2e0
Lukasz <lukasz.koltun@op.pl>
2019-06-07 23:56:45 +0200

    fetch metod added

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+        fetch('http://api.nbp.pl/api/exchangerates/rates/A/EUR/').then(resp => resp.text()).then(exRate=>{
+            console.log(exRate);
+        });
+    }    
     render(){
         return <div>
             Hello World!
